6d56eb723c96384d933d994780cd3a27c873846d
mrkucher83 <mr.kucher83@mail.ru>
2021-03-25 15:42:35 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	phrase := "Hello, OTUS!"
+
+	fmt.Println(stringutil.Reverse(phrase))
 }
