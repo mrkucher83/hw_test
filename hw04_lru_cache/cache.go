3560006d35364dc9ce48dd078585cb7b56d203ab
mrkucher83 <mr.kucher83@mail.ru>
2021-04-06 19:48:13 +0300

    HW4 not yet done

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -21,6 +21,34 @@ type cacheItem struct {
 	value interface{}
 }
 
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	if _, ok := l.items[key]; ok {
+		l.items[key].Value = value
+		l.queue.MoveToFront(l.items[key])
+		return true
+	}
+
+	l.items[key] = l.queue.PushFront(value)
+	if l.queue.Len() > l.capacity {
+		l.queue.Remove(l.queue.Back())
+		// todo: удалить значение из словаря
+	}
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	if _, ok := l.items[key]; ok {
+		l.queue.MoveToFront(l.items[key])
+		return l.items[key].Value, true
+	}
+
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.items = nil
+}
+
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "log"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,8 +19,95 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	CountItems int // счетчик элементов списка
+	FrontItem  *ListItem
+	BackItem   *ListItem
+}
+
+// реализуем методы интерфейса List для структуры list.
+func (l list) Len() int {
+	return l.CountItems
+}
+
+func (l list) Front() *ListItem {
+	return l.FrontItem
+}
+
+func (l list) Back() *ListItem {
+	return l.BackItem
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	var front *ListItem
+
+	if l.CountItems == 0 {
+		front = &ListItem{v, nil, nil}
+		l.FrontItem = front
+		l.BackItem = front
+	} else {
+		front = &ListItem{v, l.FrontItem, nil}
+		front.Next.Prev = front
+		l.FrontItem = front
+	}
+
+	l.CountItems++
+	return front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	var back *ListItem
+
+	if l.CountItems == 0 {
+		back = &ListItem{v, nil, nil}
+		l.FrontItem = back
+		l.BackItem = back
+	} else {
+		back = &ListItem{v, nil, l.BackItem}
+		back.Prev.Next = back
+		l.BackItem = back
+	}
+
+	l.CountItems++
+	return back
+}
+
+func (l *list) Remove(i *ListItem) {
+	if l.CountItems == 0 {
+		log.Println("no items to remove")
+		return
+	}
+	if i.Prev == nil {
+		l.FrontItem = i.Next
+		i.Next.Prev = nil
+	} else {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+	}
+	if i.Next == nil {
+		l.BackItem = i.Prev
+		i.Prev.Next = nil
+	} else {
+		i.Next.Prev = i.Prev
+		i.Prev.Next = i.Next
+	}
+	l.CountItems--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if l.CountItems == 0 {
+		log.Println("no items to move")
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	i.Prev = nil
+	i.Next = l.FrontItem
+	l.FrontItem = i
 }
 
 func NewList() List {
