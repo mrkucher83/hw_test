4b27b4672c1413e29c3784f72d238bb87a13ca4d
mrkucher83 <mr.kucher83@mail.ru>
2021-05-12 12:37:40 +0300

    HW7 is completed without progress bar

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +12,73 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+func Copy(fromPath, toPath string, limit, offset int64) error {
+	newLimit := limit
+
+	srcFile, err := os.OpenFile(fromPath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	finfo, err := srcFile.Stat()
+	if err != nil {
+		fmt.Println("err from Stat(): ", err)
+		return err
+	}
+	if finfo.Size() == 0 && finfo.Name() == "urandom" {
+		return ErrUnsupportedFile
+	}
+	if offset > finfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if newLimit == 0 {
+		newLimit = finfo.Size() - offset
+	}
+
+	tmpfile, err := os.CreateTemp("testdata", "tmpFile.txt")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpfile.Name())
+
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer newFile.Close()
+
+	buf, err := readOffset(srcFile, offset, newLimit)
+	if err != nil {
+		return err
+	}
+
+	_, err = tmpfile.Write(buf)
+	if err != nil {
+		return err
+	}
+
+	tf, err := os.OpenFile(tmpfile.Name(), os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer tf.Close()
+
+	_, err = io.CopyN(newFile, tf, newLimit)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func readOffset(file *os.File, offset, limit int64) ([]byte, error) {
+	buffer := make([]byte, limit+offset)
+	n, err := file.Read(buffer)
+	if err != nil {
+		return nil, fmt.Errorf("file read error: %w", err)
+	}
+	return buffer[offset:n], nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,63 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	fromPath := "testdata/input.txt"
+	toPath := "outFile.txt"
+
+	t.Run("offset0_limit0", func(t *testing.T) {
+		_ = Copy(fromPath, toPath, 0, 0)
+
+		b, _ := ioutil.ReadFile(toPath)
+		srcFile, _ := os.Open(fromPath)
+		defer srcFile.Close()
+		finfo, _ := srcFile.Stat()
+
+		require.Equal(t, finfo.Size(), int64(len(b)))
+	})
+
+	t.Run("limit exceeds file size", func(t *testing.T) {
+		_ = Copy(fromPath, toPath, 10000, 0)
+
+		b, _ := ioutil.ReadFile(toPath)
+		srcFile, _ := os.Open(fromPath)
+		defer srcFile.Close()
+		finfo, _ := srcFile.Stat()
+
+		require.Equal(t, finfo.Size(), int64(len(b)))
+	})
+
+	t.Run("offset0_limit10", func(t *testing.T) {
+		_ = Copy(fromPath, toPath, 10, 0)
+
+		b, _ := ioutil.ReadFile(toPath)
+		require.Equal(t, 10, len(b))
+	})
+
+	t.Run("offset3_limit9", func(t *testing.T) {
+		_ = Copy(fromPath, toPath, 9, 3)
+
+		b, _ := ioutil.ReadFile(toPath)
+		require.EqualValues(t, "Documents", string(b))
+		require.Equal(t, 9, len(b))
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy(fromPath, toPath, 10, 7000)
+
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", toPath, 0, 0)
+
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
 }
